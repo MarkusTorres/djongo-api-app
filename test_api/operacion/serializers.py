154acce6130aa064896eedf99d696d4955a9a754
MarkusTorres <31861281+MarkusTorres@users.noreply.github.com>
2024-10-04 23:09:01 -0600

    operacion serializer, add id

--- test_api/operacion/serializers.py
+++ test_api/operacion/serializers.py
@@ -6,7 +6,9 @@ class OperacionSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Operacion
-        fields = '__all__'
+        fields = ['id', 'id_tipo_operacion', 'codigo', 'status', 'direccion_inicio', 'direccion_final', 'tarifa',
+                  'fecha_inicio', 'fecha_final', 'cantidad', 'comentario', 'precio', 'nombre_referencia',
+                  'repartidor', 'historial', 'peso', 'largo', 'ancho', 'alto', 'devoluciones', 'entregas']
 
 
 class FlujoSerializer(serializers.HyperlinkedModelSerializer):
