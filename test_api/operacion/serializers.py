90b1a9df95e94ccb9a7a9834e583f4baad31923b
MarkusTorres <31861281+MarkusTorres@users.noreply.github.com>
2024-10-05 13:45:07 -0600

    re-added numero_referncia in operacion serializer

--- test_api/operacion/serializers.py
+++ test_api/operacion/serializers.py
@@ -6,9 +6,11 @@ class OperacionSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Operacion
-        fields = ['id', 'id_tipo_operacion', 'codigo', 'status', 'direccion_inicio', 'direccion_final', 'tarifa',
-                  'fecha_inicio', 'fecha_final', 'cantidad', 'comentario', 'precio', 'nombre_referencia',
-                  'repartidor', 'historial', 'peso', 'largo', 'ancho', 'alto', 'devoluciones', 'entregas']
+        fields = ['id', 'id_tipo_operacion', 'codigo', 'status', 'direccion_inicio',
+                  'direccion_final', 'tarifa', 'fecha_inicio', 'fecha_final',
+                  'cantidad', 'comentario', 'precio', 'nombre_referencia',
+                  'numero_referencia', 'repartidor', 'historial', 'peso',
+                  'largo', 'ancho', 'alto', 'devoluciones', 'entregas']
 
 
 class FlujoSerializer(serializers.HyperlinkedModelSerializer):
