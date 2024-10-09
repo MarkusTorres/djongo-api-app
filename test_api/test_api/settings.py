83d74a89d45858620326a07fd49e0fc2b7b2f496
MarkusTorres <31861281+MarkusTorres@users.noreply.github.com>
2024-10-08 19:59:32 -0600

    digital ocean app setup

--- test_api/test_api/settings.py
+++ test_api/test_api/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from django.core.management.utils import get_random_secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-51sgh#6ee$z#+)c1d@)7cdouqxm#%#b1$a^gkk$e21zjy@7#%x"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['localhost', '137.184.226.78', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 
 # Application definition
@@ -94,7 +97,7 @@ DATABASES = {
     "default": {
         "ENGINE": "djongo",
         # "NAME": BASE_DIR / "db.sqlite3",
-        "NAME": "djongo_api_test",
+        "NAME": "paqe_api_db",
     }
 }
 
