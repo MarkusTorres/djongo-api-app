b5fd6de3a8dabe92b8b3acbd850f620514cab532
MarkusTorres <31861281+MarkusTorres@users.noreply.github.com>
2024-10-09 00:35:57 -0600

    Revert "static files on settings.py"
    This reverts commit e190acd6b522bb72ebb414ab99e9b686ae52cd57.

--- test_api/test_api/settings.py
+++ test_api/test_api/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 import os
-import sys
-import dj_database_url
 from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -95,20 +93,13 @@ WSGI_APPLICATION = "test_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        "default": {
-            "ENGINE": "djongo",
-            # "NAME": BASE_DIR / "db.sqlite3",
-            "NAME": "paqe_api_db",
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "djongo",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": "paqe_api_db",
     }
-elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-    if os.getenv("DATABASE_URL", None) is None:
-        raise Exception("DATABASE_URL environment variable not defined")
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-    }
+}
 
 
 # Password validation
@@ -146,7 +137,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
