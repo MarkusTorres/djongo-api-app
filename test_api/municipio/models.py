310965eeade4ea40795f13bb55ee86dffb7057e4
MarkusTorres <31861281+MarkusTorres@users.noreply.github.com>
2024-10-09 20:01:47 -0600

    add custom id field on municipios

--- test_api/municipio/models.py
+++ test_api/municipio/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Municipio(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
     nombre = models.CharField(max_length=50, default='', blank=False)
     precio = models.DecimalField(max_length=6, decimal_places=2, max_digits=6)
 
