cecd30630f4695d6193fb8232864dab0b0fc955c
MarkusTorres <31861281+MarkusTorres@users.noreply.github.com>
2024-12-13 21:29:43 -0600

    enable debug

--- test_api/test_api/settings.py
+++ test_api/test_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
