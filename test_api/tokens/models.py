16462ac77e2e3feb88a78fd0ad5dc7d1bb773128
MarkusTorres <31861281+MarkusTorres@users.noreply.github.com>
2024-12-17 08:17:31 -0600

    log-in/log-out

--- test_api/tokens/models.py
+++ test_api/tokens/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Tokens(models.Model):
-    user_id = models.IntegerField(unique=True)
+    user_id = models.CharField(max_length=50)
     token = models.CharField(max_length=50)
     created = models.DateField(auto_now_add=True)
 
--- test_api/tokens/views.py
+++ test_api/tokens/views.py
@@ -1,4 +1,5 @@
 from tokens.models import Tokens
+from empleado.models import Empleado
 from tokens.serializers import TokensSerializer
 from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
@@ -7,6 +8,11 @@ from rest_framework.response import Response
 from django.http import Http404
 from rest_framework.decorators import action
 import hashlib
+from django.core.exceptions import ObjectDoesNotExist
+import jwt
+import time
+import datetime as dt
+from test_api.settings import SECRET_KEY
 
 
 # Create your views here.
@@ -29,24 +35,55 @@ class AuthViewSet(viewsets.ModelViewSet):
     def retrieve(self, request, *args, **kwargs):
         raise Http404
 
+    def _header_exists(self, headers, value):
+        if value not in headers.keys():
+            return None
+        return headers[value]
+
     @action(detail=False, methods=['get'])
     def log_in(self, request, pk=None):
-        # shall create the token and add it to the table
-        # hashlib.sha256(b"Nobody inspects the spammish repetition").hexdigest() -> hash to use
-        print("\ntoken log-in\n")
-        # breakpoint()
-        # request.META.get('HTTP_USER')
-        # request.META['HTTP_USER']
-        user = request.META['HTTP_USER']
-        gen_hash = hashlib.sha256(user).hexdigest()
-        print(gen_hash)
+        user_data = self._header_exists(request.META, 'HTTP_USER')
+        pass_data = self._header_exists(request.META, 'HTTP_PASS')
+        users = None
+        if not user_data or not pass_data:
+            return Response(data="Credentials not specified", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # check if user exists
+        try:
+            users = Empleado.objects.filter(nombre__exact=user_data).get()
+        except ObjectDoesNotExist:
+            return Response(data="User not found", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+        token_exists = Tokens.objects.filter(user_id__exact=user_data).exists()
+        # if it does not exist create the token entry and return the token data
+        # if it does indeed exist return Error
+        if token_exists:
+            return Response(data="Token for user already created", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # create the entry on DB
+        data = request.data
+        id = Tokens.objects.count() + 1
+        user_encoded_token = jwt.encode({"user": user_data}, SECRET_KEY + str(time.time()), algorithm="HS256")
+        new_token = Tokens.objects.create(
+            id=id,
+            user_id=user_data,
+            token=user_encoded_token,
+            created=dt.datetime.now().strftime('%Y-%m-%d')
+        )
+        new_token.id = id
+        new_token.save()
+        serializer = TokensSerializer(new_token)
+        # TODO: add cp on 'operacion', delete dates on 'empleado' and create and endpoint
+        #  for checking if a code is already created on operacion
+        return Response(serializer.data)
 
-    @action(detail=True, methods=['post'])
+    @action(detail=False, methods=['post'])
     def log_out(self, request):
-        # retrieve the token from header, validate it and delete the register
-        # for header use request.META.get('attribute')
-        pass
+        token_data = self._header_exists(request.META, 'HTTP_TOKEN')
+        token_exists = Tokens.objects.get(token=token_data)
+        if token_exists is None:
+            return Response(data="Token is not valid", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        token_obj = Tokens.objects.filter(token__exact=token_data).get()
+        token_obj.delete()
+        return Response(data="Token successfully deleted", status=status.HTTP_200_OK)
 
 @api_view
 def api_root(request, format=None):
