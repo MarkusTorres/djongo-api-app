de2bf122b995b26c820763816dee8dba0069552b
MarkusTorres <31861281+MarkusTorres@users.noreply.github.com>
2025-01-07 23:42:08 -0600

    middleware cors options

--- test_api/test_api/settings.py
+++ test_api/test_api/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import os
 from django.core.management.utils import get_random_secret_key
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,6 +67,13 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOW_HEADERS = (
+    *default_headers,
+    'token',
+    'usuario',
+    'pass',
+)
 
 CORS_ALLOW_ALL_ORIGINS = True
 
