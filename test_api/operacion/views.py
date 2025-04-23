5ae98ccf94a46c8c37eab772e2a668b2cd42fefb
MarkusTorres <31861281+MarkusTorres@users.noreply.github.com>
2025-04-22 22:42:18 -0600

    repartidores_global fix

--- test_api/operacion/views.py
+++ test_api/operacion/views.py
@@ -310,8 +310,10 @@ class OperacionViewSet(base_utils.GenericViewSetAuth):
         group_tipo = Operacion.objects.values('repartidor', 'id_tipo_operacion').annotate(db_count=Count('status')).order_by()
         status_counts = get_sum_object(group_status, 'status')
         group_tipo = get_sum_object(group_tipo, 'id_tipo_operacion')
+        # set(A) - (set(A) - set(B))
+        valid_repartidores = set(repartidores.keys()) - (set(repartidores.keys()) - set(status_counts.keys()))
 
-        for repartidor in repartidores.keys():
+        for repartidor in valid_repartidores:
             item = {
                 'repartidor': repartidores[repartidor],
                 'tipos': {
