5a68d63d53090d4628939a18ba7794ae9fbec7c0
MarkusTorres <31861281+MarkusTorres@users.noreply.github.com>
2025-05-11 00:41:50 -0600

    proveedor quick fix

--- test_api/proveedores/views.py
+++ test_api/proveedores/views.py
@@ -18,8 +18,8 @@ class ProveedorViewSet(base_utils.GenericViewSetAuth):
             id=base_utils.get_model_new_id(Proveedor),
             nombre=data['nombre'],
             tarifa=data['tarifa'],
-            tarifa_foraneo=data['tarifa_foraneo'],
-            tarifa_local=data['tarifa_local']
+            tarifa_contacto_efectivo=data['tarifa_contacto_efectivo'],
+            tarifa_repartidor=data['tarifa_repartidor']
         )
         serializer = ProveedorSerializer(new_item)
         return Response(serializer.data)
@@ -52,8 +52,8 @@ class ProveedorViewSet(base_utils.GenericViewSetAuth):
         proveedor_obj = Proveedor.objects.filter(id__exact=data['id']).get()
         proveedor_obj.nombre = base_utils.value_or_default('nombre', data, proveedor_obj.nombre)
         proveedor_obj.tarifa = base_utils.value_or_default('tarifa', data, proveedor_obj.tarifa)
-        proveedor_obj.tarifa_foraneo = base_utils.value_or_default('tarifa_contacto_efectivo', data, proveedor_obj.tarifa_foraneo)
-        proveedor_obj.tarifa_local = base_utils.value_or_default('tarifa_repartidor', data, proveedor_obj.tarifa_local)
+        proveedor_obj.tarifa_contacto_efectivo = base_utils.value_or_default('tarifa_contacto_efectivo', data, proveedor_obj.tarifa_foraneo)
+        proveedor_obj.tarifa_repartidor = base_utils.value_or_default('tarifa_repartidor', data, proveedor_obj.tarifa_local)
 
         proveedor_obj.save()
         serialized_obj = ProveedorSerializer(proveedor_obj)
