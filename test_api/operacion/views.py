54a2133767d0d5c9d40d725f573e012002159247
MarkusTorres <31861281+MarkusTorres@users.noreply.github.com>
2025-11-04 23:57:20 -0600

    added id_cliente in operacion/filtro/

--- test_api/operacion/views.py
+++ test_api/operacion/views.py
@@ -258,20 +258,21 @@ class OperacionViewSet(base_utils.GenericViewSetAuth):
         try:
             data = request.data
 
-            if data['codigos'] is not None:
-                resp = Operacion.objects.filter(codigo__in=data['codigos'])
-                return resp
+            if 'codigos' in data.keys():
+                if data['codigos'] is not None:
+                    resp = Operacion.objects.filter(codigo__in=data['codigos'])
+                    return resp
 
             fecha_1 = datetime.datetime.strptime(data['fecha1'], '%Y-%m-%d') if data['fecha1'] else None
             fecha_2 = datetime.datetime.strptime(data['fecha2'], '%Y-%m-%d') if data['fecha2'] else None
             finalizada = base_utils.value_or_default('finalizada', data, False)
 
             queries_list = [
-                Operacion.objects.filter(id_tipo_operacion=data['id_tipo_operacion']) if data['id_tipo_operacion'] else None,
+                Operacion.objects.filter(id_tipo_operacion=data['id_tipo_operacion']) if ('id_tipo_operacion' in data.keys()) else None,
                 Operacion.objects.filter(finalizada__in=[finalizada]),
-                # Operacion.objects.filter(codigo=data['codigo']) if data['codigo'] else None,
-                Operacion.objects.filter(status=data['status']) if data['status'] else None,
-                Operacion.objects.filter(repartidor=data['repartidor']) if data['repartidor'] else None,
+                Operacion.objects.filter(id_cliente=data['id_cliente']) if ('id_cliente' in data.keys()) else None,
+                Operacion.objects.filter(status=data['status']) if ('status' in data.keys()) else None,
+                Operacion.objects.filter(repartidor=data['repartidor']) if ('repartidor' in data.keys()) else None,
                 Operacion.objects.filter(fecha_inicio__range=(fecha_1, fecha_2)) if (fecha_1 and fecha_2) else None
             ]
             if finalizada is None:
